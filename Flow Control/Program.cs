64423028154d6150229edfaecb844063a19d5b72
Isklett <isak.lagerqvist@gmail.com>
2026-05-21 15:53:52 +0200

    Project Start

---
+++ Flow Control/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Flow_Control
+{
+    internal class Program
+    {
+        private static bool _running = true;
+        static void Main(string[] args)
+        {
+            while (_running)
+            {
+
+            }
+            Environment.Exit(0);
+        }
+    }
+}
